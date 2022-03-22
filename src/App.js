18f3db2de74aecdb91513ebd20b464a4d31bdf2e
Genie <forgenie4all@gmail.com>
2022-03-22 22:37:38 +0800

    setting routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import './App.css'
+import HomePage from './pages/HomePage'
+import MemberPage from './pages/MemberPage'
+import EditMemberPage from './pages/EditMemberPage'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/member" element={<MemberPage />} />
+        <Route exact path="/member/edit" element={<EditMemberPage />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App

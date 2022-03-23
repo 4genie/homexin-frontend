d32fa3dc25d80b2cb96f3e83b294d43058d8c4f8
Genie <forgenie4all@gmail.com>
2022-03-23 22:59:50 +0800

    finish editMemberPage

--- src/styles/breakpoints.js
+++ src/styles/breakpoints.js
@@ -1,3 +1,3 @@
-export const MEDIA_QUERY_SM = '@media only screen and (max-width: 376px)'
+export const MEDIA_QUERY_SM = '@media only screen and (max-width: 375px)'
 export const MEDIA_QUERY_MD = '@media only screen and (max-width: 768px)'
 export const MEDIA_QUERY_LG = '@media only screen and (max-width: 1024px)'
